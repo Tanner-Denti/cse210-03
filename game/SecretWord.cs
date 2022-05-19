626620416ef13e9dfcf866a881a7c5ba351b45ff
Drew LaMunyon <galpha7@gmail.com>
2022-05-19 11:10:14 -0600

    Added txt file to contain words

--- game/SecretWord.cs
+++ game/SecretWord.cs
@@ -2,14 +2,29 @@ namespace game
 {
     public class SecretWord
     {
-
-
+        string[] words;
+        private List<string> wordlist = new List<string>();
+        int index;
+        string word;
 
         public SecretWord()
         {
-
+            Random random = new Random();
+            words = System.IO.File.ReadAllLines("words.txt");
+            word = " ";
+            foreach (string word in words)
+            {
+                wordlist.Add(word);
+            }
+            
         }
 
+        private string ChooseWord()
+        {
+            index = Random.Next(wordlist.Count);
+            word = wordlist[index];
+            return word;
+        }
 
         
     }
